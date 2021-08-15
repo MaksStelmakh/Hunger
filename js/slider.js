92ddef20dc9af7211ddf24419f66daa3263e5785
MaksStelmakh <step4ick2017@gmail.com>
2021-08-15 23:48:43 +0300

    Добавил JS

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+        $('.specialties-slider').slick({
+            arrows:false,
+            dots:true,
+            autoplay: true,
+            autoplaySpeed: 5000
+        });
+});
+    
+
+type="text/javascript">
+    $(document).ready(function () {
+        $('.modal-gallery').slick({
+            arrows: true,
+            dots: true,
+        });
+    });
